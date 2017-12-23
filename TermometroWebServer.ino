6593edb7daa508db5aa58884efd4f07abddbf151
Amicojeko <jeko@jeko.net>
2017-12-23 17:24:53 +0100

    first commit

---
+++ TermometroWebServer.ino
@@ -0,0 +1,116 @@
+/*
+* MIT License
+*
+* Copyright (c) 2017 Stefano Guglielmetti
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#include "LedControl.h"
+#include <DHT.h>;
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#define DHTPIN   D1
+#define DHTTYPE  DHT22
+#define LCD_TEMP 0
+#define LCD_HUM  1
+#define LCD_HUM  1
+#define DATAPIN  D2
+#define CLKPIN   D4
+#define LOADPIN  D3
+#define LEDPIN   D5
+
+const char *ssid = "<WIFI SSID>";
+const char *password = "<WIFI PASSWORD>";
+
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+LedControl lc=LedControl(DATAPIN, CLKPIN, LOADPIN, 1); // Initialize LCD
+ESP8266WebServer server ( 80 ); // Initialize web server
+
+unsigned long Timer=0; // will be used by the non-blocking timer
+
+float hum;  // Stores humidity value
+float temp; // Stores temperature value
+
+void setup() {
+  pinMode ( LEDPIN, OUTPUT ); // Initialize LED Pin
+  digitalWrite ( LEDPIN, 0 ); // Ensure the LED is off
+
+  Serial.begin ( 115200 ); // Start serial
+  WiFi.begin ( ssid, password ); // Connect to WiFi
+  dht.begin(); // Start DHT
+
+  lc.shutdown(0,false);
+  lc.setIntensity(0,8); // Set LCD Intensitiy to a medium value
+  lc.clearDisplay(0); // Wipe LCD Screen
+
+  Serial.println ("Termometro Started");
+
+  // Wait for connection
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  // Some debug, useful to retrieve the IP Address
+  Serial.println ( "" );
+  Serial.print   ( "Connected to " );
+  Serial.println ( ssid );
+  Serial.print   ( "IP address: " );
+  Serial.println ( WiFi.localIP() );
+
+  if ( MDNS.begin ( "esp8266" ) ) {
+    Serial.println ( "MDNS responder started" );
+  }
+
+  // Termometro web interface will be available at
+  // http://<termometro IP address>/
+
+  server.on ( "/", handleRoot ); // Assign "/" route to the handleRoot(); function
+  server.onNotFound ( handleNotFound ); // Assign not found route (404) to the handleNotFound (); function
+
+  server.begin(); // Start HTTP Server
+  Serial.println ( "HTTP server started" );
+}
+
+void loop() {
+  server.handleClient(); // The HTTP server is listening constantly
+
+  // This is a non blocking timer: Every 5000ms (5sec) it reads the
+  // humidity and temperature from the DHT22, and sends them to the LCD
+  if ( (millis() - Timer) >= 5000) {
+    Timer = millis();
+
+    hum  = dht.readHumidity();
+    temp = dht.readTemperature();
+
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+
+    sendToLCD(temp * 10, LCD_TEMP);
+    sendToLCD(hum * 10, LCD_HUM);
+  }
+
+}
---
+++ routes.ino
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2015, Majenko Technologies
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+void handleRoot() {
+  digitalWrite ( LEDPIN, 1 );
+  char tmp[800];
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+  char temp_buff[10];
+  char hum_buff[10];
+
+  snprintf ( tmp, 800,
+
+"<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>Termometro</title>\
+    <style>\
+      body { background-color: #000000; font-family: Arial, Helvetica, Sans-Serif; Color: #FFFFFF; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>\
+      Temp: %2s &#8451;\
+      <br>\
+      Hum: %2s %\
+    </h1>\
+  </body>\
+</html>",
+
+    dtostrf(dht.readTemperature(), 4, 1, temp_buff), 
+    dtostrf(dht.readHumidity(), 4, 1, hum_buff)
+  );
+  server.send ( 200, "text/html", tmp );
+  digitalWrite ( LEDPIN, 0 );
+}
+
+void handleNotFound() {
+  digitalWrite ( LEDPIN, 1 );
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+
+  server.send ( 404, "text/plain", message );
+  digitalWrite ( LEDPIN, 0 );
+}
